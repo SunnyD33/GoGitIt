4dbedb3da12f73209511cd00f12baf57c175fe28
Quincy Williams <QLWilliams@student.fullsail.edu>
2022-11-01 10:10:26 -0400

    Initial build

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"os"
+)
+
+type config struct {
+	printHelpText bool
+}
+
+var helpText = "Help text should print"
+
+func printHelpText(w io.Writer) {
+	fmt.Fprintf(w, helpText)
+	fmt.Println("")
+}
+
+func parseArgs(args []string) (config, error) {
+	c := config{}
+
+	if len(args) != 1 {
+		return c, errors.New("Invalid number of arguments\n")
+	}
+
+	if args[0] == "-h" || args[0] == "--help" {
+		c.printHelpText = true
+		return c, nil
+	}
+
+	return c, nil
+}
+
+func runCmd(r io.Reader, w io.Writer, c config) error {
+	if c.printHelpText {
+		printHelpText(w)
+		return nil
+	}
+
+	fmt.Println("Command ran successfully")
+	return nil
+}
+
+func main() {
+	c, err := parseArgs(os.Args[1:])
+	if err != nil {
+		fmt.Fprintln(os.Stdout, err)
+		printHelpText(os.Stdout)
+		os.Exit(1)
+	}
+
+	err = runCmd(os.Stdin, os.Stdout, c)
+	if err != nil {
+		fmt.Fprintln(os.Stdout, err)
+		os.Exit(1)
+	}
+}
