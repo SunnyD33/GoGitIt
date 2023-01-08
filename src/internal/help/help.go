2849520cdc5b3b6a541b5079a0197e97caba1ea5
Quincy Williams <QLWilliams@student.fullsail.edu>
2023-01-07 22:40:09 -0500

    updated help function

--- src/internal/help/help.go
+++ src/internal/help/help.go
@@ -7,11 +7,9 @@ import (
 func HelpCommand() {
     fmt.Println("Commands:")
     fmt.Println("-h / --help \n  Shows help for available commands. Can be used for each command as well")
-    fmt.Println("-a  \n  Authorize yourself to increase rate limit of use and access some commands that requires auth")
-    fmt.Println("-r  \n  Searches for the repo that was entered by username")
-    fmt.Println("-s \n  Checks your auth status")
-    fmt.Println("-o \n  Opens an enetered repo on your browser")
-    fmt.Println("--setenv \n allows you to set where the tool will read data from your .env file")
+    fmt.Println("setup  \n  Allows user to check current auth status, authorize themselves and change .env file location")
+    fmt.Println("list  \n  Allows user to list repos for an entered username")
+    fmt.Println("open \n  Opens an enetered repo on your browser")
     fmt.Println("search \n  Searches for repos based on entered criteria")
     fmt.Println("rate \n  Shows your current rate limit")
 }
